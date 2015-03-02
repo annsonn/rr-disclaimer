559ef47db72d6b1cc4a3b854cd4deddccd3573aa
Anson Chung <anson.chung@gmail.com>
2015-03-02 03:14:36 +0000

    setting date and submit button

--- client/app/waiver/waiver.controller.js
+++ client/app/waiver/waiver.controller.js
@@ -2,18 +2,15 @@
 
 angular.module('rrDisclaimerApp')
   .controller('WaiverCtrl', function ($scope) {
-    
-    $scope.days = [1,2,3,4,5,6,7,8,9,10,
-                  11,12,13,14,15,16,17,18,19,20,
-                  21,22,23,24,25,26,27,28,29,30,
-                  31];
-    $scope.months = ['January', 'February', 'March', 'April', 'May', 'June', 
-                     'July', 'August', 'September', 'October', 'November', 'December'];
-    $scope.years = [2015,2016];
-    
-    $scope.user = {
-      signDay: new Date().getDate(),
-      signMonth: $scope.months[new Date().getMonth()],
-      signYear: new Date().getFullYear()
+           
+    $scope.user = {      
+      date: new Date()
+    };
+        
+    $scope.submit = function() {
+      $scope.submitAttempted = true;
+      console.log('waiverform', $scope.waiverForm);
+      
+      console.log('submitting', $scope.user);
     };
   });
