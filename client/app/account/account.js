9e563bab8853f6be13f815cd7fdcfaf5bede4514
Anson Chung <anson.chung@gmail.com>
2015-03-14 19:16:55 +0000

    styling password change page and removing test user from seed, and disabling signup page

--- client/app/account/account.js
+++ client/app/account/account.js
@@ -7,13 +7,17 @@ angular.module('rrDisclaimerApp')
         templateUrl: 'app/account/login/login.html',
         controller: 'LoginCtrl'
       })
-      .when('/signup', {
-        templateUrl: 'app/account/signup/signup.html',
-        controller: 'SignupCtrl'
-      })
+      
       .when('/settings', {
         templateUrl: 'app/account/settings/settings.html',
         controller: 'SettingsCtrl',
         authenticate: true
       });
   });
+
+/*
+.when('/signup', {
+        templateUrl: 'app/account/signup/signup.html',
+        controller: 'SignupCtrl'
+      })
+*/
